60bc9dbe4c8cf6deb23d7bb8f1277af654f6ff97
floRaths <raths.f@gmail.com>
2025-09-20 21:11:49 -0700

    test

--- auxFns.py
+++ auxFns.py
@@ -2,8 +2,29 @@ import subprocess
 
 
 def get_proj_version():
-    result = subprocess.run(['uv', 'version', '--short'], capture_output=True, text=True)
-    return result.stdout
+    result = subprocess.run(['uv', 'version', '--short', '--color', 'never'], capture_output=True, text=True)
+    return result.stdout.rstrip('\n')
+
 
 def bump_proj_version(which):
-    subprocess.run(['uv', 'version', "--bump", which])
+    subprocess.run(['uv', 'version', '--bump', which])
+
+
+def run_command(command):
+    c_expanded = command.split(' ')
+
+    result = subprocess.run(
+        c_expanded,
+        capture_output=True,
+        text=True,
+    )
+
+    success = True if result.returncode == 0 else False
+
+    print(result.stdout)
+
+    if result.returncode != 0:
+        print('Exit code:', result.returncode)
+        print('Error:', result.stderr)
+
+    return result, success
