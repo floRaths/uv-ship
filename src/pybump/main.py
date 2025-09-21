8f6c4f6ff88cd7d331cba983066f9e19134798b1
floRaths <raths.f@gmail.com>
2025-09-20 22:11:38 -0700

    main

--- src/pybump/main.py
+++ src/pybump/main.py
@@ -2,7 +2,7 @@ import argparse
 import subprocess
 
 
-def run_command(command):
+def run_command(command, print_stdout=True):
     c_expanded = command
 
     result = subprocess.run(
@@ -13,7 +13,9 @@ def run_command(command):
 
     success = True if result.returncode == 0 else False
 
-    print(result.stdout)
+    if print_stdout:
+        if len(result.stdout) > 0:
+            print(result.stdout)
 
     if result.returncode != 0:
         print('Exit code:', result.returncode)
@@ -23,28 +25,36 @@ def run_command(command):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Bump version, create git tag, and push changes.")
-    parser.add_argument("bump", choices=["major", "minor", "patch"], help="Type of version bump.")
+    parser = argparse.ArgumentParser(description='Bump version, create git tag, and push changes.')
+    parser.add_argument('bump', choices=['major', 'minor', 'patch'], help='Type of version bump.')
     args = parser.parse_args()
 
     bump = args.bump
 
-    result, success = run_command(['uv', 'version', '--short', '--color', 'never'])
-    current_version = result.stdout.strip()
+    result, success = run_command(['uv', 'version', '--color', 'never'], print_stdout=False)
+    current_version = result.stdout.strip().split(' ')
+    project_name = current_version[0]
+    current_version = current_version[1]
 
-    run_command(['uv', 'version', '--bump', bump])
+    print(f'Bumping "{project_name}" to the next {bump} version.')
 
-    result, success = run_command(['uv', 'version', '--short', '--color', 'never'])
+    run_command(['uv', 'version', '--bump', bump], print_stdout=True)
+
+    result, success = run_command(['uv', 'version', '--short', '--color', 'never'], print_stdout=False)
     new_version = result.stdout.strip()
 
     TAG = f'v{new_version}'
     MESSAGE = f'new version: {current_version} → {new_version}'
 
+    print('committing updated pyproject.toml and uv.lock files')
     result, success = run_command(['git', 'add', 'pyproject.toml', 'uv.lock'])
-    result, success = run_command(['git', 'commit', '-m', MESSAGE])
+    result, success = run_command(['git', 'commit', '-m', MESSAGE], print_stdout=False)
+    print(f'creating git tag: {TAG}')
     result, success = run_command(['git', 'tag', TAG, '-m', MESSAGE])
+    print('pushing changes to remote repository')
     result, success = run_command(['git', 'push'])
     result, success = run_command(['git', 'push', 'origin', TAG])
+    print('✅ Done! New version registered and tagged.')
 
 
 # if __name__ == '__main__':
